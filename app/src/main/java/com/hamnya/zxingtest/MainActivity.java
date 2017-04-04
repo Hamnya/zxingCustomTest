fa2a1226a9147a6986848926944695628c7dba7d
Hanna <ihamnya@gmail.com>
2017-04-04 20:37:33 +0900

    Initial commit

---
+++ app/src/main/java/com/hamnya/zxingtest/MainActivity.java
@@ -0,0 +1,25 @@
+package com.hamnya.zxingtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+
+/**
+ * This is barcode scanner Test using zximg library
+ * 출처: https://github.com/journeyapps/zxing-android-embedded/
+ * */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void scanCustomScanner(View view) {
+        new IntentIntegrator(this).setOrientationLocked(false).setCaptureActivity(CustomScannerActivity.class).initiateScan();
+    }
+
+}
