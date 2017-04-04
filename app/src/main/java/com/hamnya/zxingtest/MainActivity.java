096f1b15d4582ad96988ebd0c6dd1ab51817ba96
Hanna <ihamnya@gmail.com>
2017-04-04 20:50:37 +0900

    add result

--- app/src/main/java/com/hamnya/zxingtest/MainActivity.java
+++ app/src/main/java/com/hamnya/zxingtest/MainActivity.java
@@ -1,10 +1,14 @@
 package com.hamnya.zxingtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 /**
  * This is barcode scanner Test using zxing library
@@ -20,6 +24,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void scanCustomScanner(View view) {
         new IntentIntegrator(this).setOrientationLocked(false).setCaptureActivity(CustomScannerActivity.class).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.d("MainActivity", "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d("MainActivity", "Scanned");
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 }
